39149340ee8d3e3bd01ca247b21dbcffe865dab3
Eli-Yu <puzhuyu@hotmail.com>
2023-04-15 18:07:40 +0800

    FormHeader hard code replace

--- mage/Editors/FormHeader.cs
+++ mage/Editors/FormHeader.cs
@@ -215,8 +215,10 @@ namespace mage
             }
             catch (Exception ex)
             {
-                MessageBox.Show("One of the values entered was not valid.\n\n" + ex.Message,
-                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //MessageBox.Show("One of the values entered was not valid.\n\n" + ex.Message,
+                //        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(Properties.Resources.formValueNotValidErrorText + ex.Message,
+                        Properties.Resources.form_ErrorMessageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
